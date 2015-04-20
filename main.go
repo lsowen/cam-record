554fd5b9ab4124a60eb702c8a42268708094d87a
lsowen <logan@s1network.com>
2015-04-20 08:34:55 -0400

    add ability to configure listen port

--- main.go
+++ main.go
@@ -139,8 +139,9 @@ type Configuration struct {
 }
 
 func main() {
-	var configLocation string
+	var configLocation, listenOn string
 	flag.StringVar(&configLocation, "config", "", "config location")
+	flag.StringVar(&listenOn, "listen", ":8080", "IP/port to listen on")
 	flag.Parse()
 
 	configBytes, err := ioutil.ReadFile(configLocation)
@@ -220,6 +221,6 @@ func main() {
 	)
 
 	if err == nil {
-		http.ListenAndServe(":8080", &handler)
+		http.ListenAndServe(listenOn, &handler)
 	}
 }
